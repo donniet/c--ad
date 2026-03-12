9f907a44896398e9dc43e77d9212f000f843a22e
Donnie Tolbert <donniet@gmail.com>
2026-03-12 17:50:06 -0500

    cleanup

--- test/test_expressions.cpp
+++ test/test_expressions.cpp
@@ -1,48 +1,106 @@
-/**
- * test of expressions.hpp
- */
-
 #include "expressions.hpp"
+#include "units.hpp"
 
 #include <print>
+#include <format>
+#include <cassert>
 
-int main( int ac, char * av[] ) 
+
+int main( int ac, char* av[] )
 {
-    using namespace expressions;
-    using namespace expressions::operators;
     using std::println;
 
-    auto zero = constant_zero< double >;
-    auto one = constant_one< double >;
+    using namespace expressions;
+    using namespace units;
 
-    set_variable_value< double, 31415 >( std::numbers::pi );
-    auto pi = get_variable_value< double, 31415 >();
+    auto zero = constant_zero;
+    auto one = constant_one;
 
-    if( pi != (double)std::numbers::pi )
-        throw std::logic_error( "get_variable_value returned incorrect value" );
+    auto vars = declare_variables(
+        var< long double >( "x" ), 
+        var< long double >( "y" ),
+        var< Length      >( "l" ),
+        var< Velocity    >( "v" ),
+        var< Scalar      >( "a" ));
 
-#ifndef NDEBUG
-    println( "retrieved value from <31415, double> is {}", pi );
-#endif
+    auto [ x, y, l, v, a ] = vars.all();
 
-    set_variable_value< double, 31415 >( 22. / 7. );
-    pi = get_variable_value< double, 31415 >();
+    auto d_x = differential( x );
+    auto d_y = differential( y );
+    auto d_l = differential( l );
+    auto d_v = differential( v );
+    auto d_a = differential( a );
 
-    if( pi != (double)( 22. / 7. ))
-        throw std::logic_error( "get_variable_value returned incorrect value" );
+    println( "{}", zero );
 
-#ifndef NDEBUG
-    println( "retrieved value from <31415, double> after a reset is {}", pi );
-#endif
+    auto f = ( 5 + zero + one );
 
-    auto x0 = Variable< double, 0 >{};
-    auto fx = sin( x0 );
-    auto gx = cos( x0 );
+    println( "{}", eval( f ));
+    println( "{}", x );
+    println( "{}", x * one );
+    println( "{}", x + one );
+    println( "{}", x - one );
+    println( "{}", -x );
+    println( "{}", ( x == one ) );
+    println( "{}", ( x == one and x == zero ) );
+    
+    x = 8.l;
 
-    x0.set( pi / 2. );
+    auto g = ( 5 + 3*x - f );
+    println( "{}", eval( g ));
+    println( std::runtime_format( "g == {}" ), g );
 
-    println( "sin( pi / 2 ) == {}", fx() );
-    println( "cos( pi / 2 ) == {}", gx() );
+    auto dg = d_x( g );
+    println( "{}", dg );
+    println( "g() == {}", eval( dg ));
+
+    l = 12_in;
+
+    auto h = ( 1_sqft - l * l ) / 254_mm;
+    println( "h({}) == {}", l, h );
+    println( "h({}) == {:ft}", 12_in, eval( h ));
+
+    auto dh = d_l( h );
+    println( "{}", dh );
+    println( "dh({}) == {}", 12_in, eval( dh ));
+
+    auto t1 = make_tuple( 3_mm / 1_s, 5_mm / 1_s, 2_mm / 1_s );
+    auto t2 = make_tuple( 3_mm / 1_s, 5_mm / 1_s, v );
+    auto s1 = make_tensor< Shape< 3 >>( 3_mm / 1_s, 5_mm / 1_s, 2_mm / 1_s );
+    auto s2 = make_tensor< Shape< 3 >>( 3_mm / 1_s, 5_mm / 1_s, v );
+
+    auto eq = ( t1 == t2 );
+
+    v = 2_mm / 1_s;
+
+    assert( eval( eq ));
+
+    v = 2_ft / 1_s;
+
+    assert( not eval( eq ));
+
+    auto m1 = make_tensor< Shape< 2, 2 >>( 
+        a, -a,
+        a, a );
+
+    a = 1_scalar;
+
+    println( "det(1,-1,1,1) == {}", eval( det( m1 ) ));
+    assert( eval( det( m1 ) ) == 2_scalar );
+    assert( eval( d_a( det( m1 )) ) == 4_scalar );
+
+    auto grad = gradient( a );
+
+    auto para = ( pow< 2 >( x - 2 ) + pow< 2 >( y - 3 ) + 3 );
+
+    auto solver = gradient_descent( x, y );
+    solver[ maximum_iterations ] = 1000;
+    solver[ learning_rate ] = 1e-3;
+
+    solver( para );
+    println( "solved: para({}, {}) == {}", eval( x ), eval( y ), eval( para ));
+
+
 
 
     return EXIT_SUCCESS;
