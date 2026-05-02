4dd928d8187dad6d8cf1ad74b8166bf42a544206
Donnie Tolbert <donniet@gmail.com>
2026-05-02 11:49:29 -0500

    further normalization tests

--- test/test_normalize.cpp
+++ test/test_normalize.cpp
@@ -26,13 +26,28 @@ int main( int ac, char* av[] )
     // ~(~F|~T)&~F
     // (F&T)&~F == F
 
+    // TEST: ( F and T ) or F => 
+    //  ( F and T ) or F =>
+    //  F or F =>
+    //  F
     constexpr auto a = or_( and_( false, true ), false );
     constexpr auto an = normalize< Or, And, Not >( a );
-    static_assert( (bool)an == (bool)a );
+    static_assert( not a and (bool)an == (bool)a );
+    static_assert( is_same_v< std::remove_cvref_t< decltype( an ) >, 
+        Or< And< bool, bool >, bool >> );
 
+    // TEST: not (( F and T ) or F ) => 
+    //  not ( F and T ) and not F =>
+    //  ( not F or not T ) and not F =>
+    //  ( not F and not F ) or ( not T and not F ) =>
+    //  ( T and T ) or ( F and T ) =>
+    //  T or F =>
+    //  T
     constexpr auto b = not_( or_( and_( false, true ), false ));
     constexpr auto bn = normalize< Or, And, Not >( b );
-    static_assert( (bool)bn == (bool)b );
+    static_assert( b and (bool)bn == (bool)b );
+    static_assert( is_same_v< std::remove_cvref_t< decltype( bn ) >, 
+        Or< And< Not< bool >, Not< bool >>, And< Not< bool >, Not< bool >>> > );
 
 
 
