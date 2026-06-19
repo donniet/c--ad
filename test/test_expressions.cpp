de46363e5f6a596b0b7280c40f09be9d6b653831
Donnie Tolbert <donniet@gmail.com>
2026-06-19 13:52:29 -0500

    breaking commit for syncing with github

--- test/test_expressions.cpp
+++ test/test_expressions.cpp
@@ -4,79 +4,7 @@
 #include "expressions/solvers.hpp"
 #include "expressions/format.hpp"
 
-#include <print>
-#include <format>
-#include <cassert>
-#include <string>
-#include <stdexcept>
-#include <stdlib.h>
-
-
-namespace test {
-
-void report( bool success, std::string const& name, std::string const& msg )
-{
-    if( not success )
-    {
-        if( msg == "" )
-            std::println( "FAILURE: {}", name );
-        else
-            std::println( "FAILURE: {}: {}", name, msg );
-
-        std::exit( EXIT_FAILURE );
-        return;
-    }
-    
-    std::println( "SUCCESS: {}", name );
-}
-
-void ensure( bool (*func)(), std::string const& name )
-{
-    bool success = false;
-    std::string msg = "";
-
-    try 
-    { if( success = func(); not success )
-        msg = "returned false"; }
-    catch( std::runtime_error& e )
-    { msg = e.what(); }
-    catch(...)
-    { msg = "exception thrown"; }
-
-    report( success, name, msg );
-}
-
-void ensure( void (*func)(), std::string const& name )
-{
-    bool success = false;
-    std::string msg = "";
-
-    try
-    { func(); success = true; }
-    catch( std::runtime_error& e )
-    { msg = e.what(); }
-    catch(...)
-    { msg = "exception thrown"; }
-
-    report( success, name, msg );
-}
-
-void ensure( std::pair< bool, std::string > (*func)(), std::string const& name )
-{
-    bool success = false;
-    std::string msg = "";
-
-    try
-    { std::tie( success, msg ) = func(); }
-    catch( std::runtime_error& e )
-    { msg = e.what(); }
-    catch(...)
-    { msg = "exception thrown"; }
-
-    report( success, name, msg );
-}
-
-} // namespace test
+#include "testing.hpp"
 
 using test::ensure;
 using namespace expressions;
@@ -87,7 +15,6 @@ bool test_minimization();
 bool test_canonicalization();
 constexpr bool test_dependent_vars();
 std::pair< bool, std::string > test_boolean_satisfaction();
-
 
 int main( int ac, char* av[] )
 {
